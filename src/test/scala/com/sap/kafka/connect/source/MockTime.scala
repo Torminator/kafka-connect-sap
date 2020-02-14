a12e90173faee0eec6ac202d3ab175c815689f35
torminator <torben.schick@adesso.de>
2020-02-14 16:32:57 +0100

    fixed for scala_2.12 (pom adjustments, mockito upgrade adjustments)

--- src/test/scala/com/sap/kafka/connect/source/MockTime.scala
+++ src/test/scala/com/sap/kafka/connect/source/MockTime.scala
@@ -1,6 +1,7 @@
 package com.sap.kafka.connect.source
 
 import java.util.concurrent.TimeUnit
+import java.util.function.Supplier
 
 import org.apache.kafka.common.utils.Time
 
@@ -19,6 +20,8 @@ class MockTime extends Time {
   }
 
   override def hiResClockMs(): Long = ???
+
+  override def waitObject(obj: Any, condition: Supplier[java.lang.Boolean], timeoutMs: Long): Unit = ???
 
   override def sleep(ms: Long): Unit = {
     this.nanos += TimeUnit.NANOSECONDS.convert(ms, TimeUnit.MILLISECONDS)
